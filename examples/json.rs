6582bca9f71522ee6a409bfbe6a9caf02b50a81c
qzed <qzed@users.noreply.github.com>
2018-03-18 03:07:46 +0100

    Add example for json-type

---
+++ examples/json.rs
@@ -0,0 +1,42 @@
+#[macro_use]
+extern crate slog;
+extern crate slog_conf;
+
+extern crate toml;
+
+use slog::Drain;
+
+use slog_conf::{Config, JsonConfig};
+
+
+fn main() {
+    // load a configuration with the default deserializers
+    let config: Box<Config> = toml::from_str(include_str!("json.toml")).unwrap();
+
+    // show what we have just loaded
+    println!("-- JsonConfig ------------");
+    println!("{:#?}\n", config.downcast_ref::<JsonConfig>().unwrap());
+
+    // show it again, this time as toml output (via serialization)
+    // Note: we have to use an intermediate `toml::Value` here because TOML
+    // requires that tables (i.e. maps, structs, or sequences) must be emitted
+    // last. We can acheive this with an intermediate `toml::Value`. This
+    // should not be necessary for other formats.
+    let val = toml::Value::try_from(&*config).unwrap();
+    let out = toml::to_string_pretty(&val).unwrap();
+    println!("-- JsonConfig as TOML ----");
+    println!("{}\n", out);
+
+    // build a logger
+    let (async, _guard) = slog_conf::build(config.as_ref()).unwrap();
+    let log = slog::Logger::root(async.fuse(), o!());
+
+    // use the logger
+    warn!(log, "a warning"; "a" => "b");
+    info!(log, "some information that might be relevant"; "b" => "c");
+
+    let log = log.new(o!("a" => "b"));
+    debug!(log, "a debug message"; "f" => "g");
+    debug!(log, "another debug message");
+    error!(log, "an error message");
+}
